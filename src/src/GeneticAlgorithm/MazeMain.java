784e853c54a97b4e56177f6e58d44ec684b49564
chiragchadha <chirag.chadha@rutgers.edu>
2018-09-29 18:39:51 -0400

    "Working part 2"

--- src/src/GeneticAlgorithm/MazeMain.java
+++ src/src/GeneticAlgorithm/MazeMain.java
@@ -4,8 +4,8 @@ import java.util.Map;
 
 public class MazeMain {
     public static void main(String[] args){
-     GA GAClass = new GA(5,0.2);
-     GAClass.start();
+     GA GAClass = new GA(10,0.2);
+     GAClass.start("Manhattan");
 
     }
 }
